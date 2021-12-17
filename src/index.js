b8e8e720993f82fe3e32c49e90719eefdf9e03a1
Elena Pavel <elena.pavel.dev@gmail.com>
2021-12-17 22:46:16 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import HelloWorld from './HelloWorld';
+
+import $ from './style.css';
+
+render(
+	<BrowserRouter>
+		<HelloWorld />
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="about" element={<About />} />
+		</Routes>
+		<HelloWorld />
+	</BrowserRouter>,
+	document.getElementById('root')
+);
+
+function Home() {
+	return (
+		<>
+			<main className={$.main}>
+				<h2>Welcome to the homepage!</h2>
+				<p>You can do this, I believe in you.</p>
+			</main>
+			<nav>
+				<Link to="/about">About</Link>
+			</nav>
+		</>
+	);
+}
+
+function About() {
+	return (
+		<>
+			<main>
+				<h2>Who are we?</h2>
+				<p>That feels like an existential question, don't you think?</p>
+			</main>
+			<nav>
+				<Link to="/">Home</Link>
+			</nav>
+		</>
+	);
+}
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
+    alias: {
+      react: path.join(__dirname, 'node_modules', 'react'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[local]_[hash:base64:5]',
+              },
+              importLoaders: 1,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
